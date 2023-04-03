aa412f4f62f77ac80571607f2ddcbe091a1994c1
Karel Rappo <karelrappo@gmail.com>
2023-04-03 19:29:37 +0200

    Change js

--- js/demo.js
+++ js/demo.js
@@ -9,20 +9,4 @@ document.addEventListener('DOMContentLoaded', function () {
     dotColor: '#5cbdaa',
     lineColor: '#5cbdaa'
   });
-  var intro = document.getElementById('intro');
-  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
 }, false);
-
-
-/*
-// jQuery plugin example:
-$(document).ready(function() {
-  $('#particles').particleground({
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
-  });
-  $('.intro').css({
-    'margin-top': -($('.intro').height() / 2)
-  });
-});
-*/
